db4422a1c4448709434f325ec70fa0fa2b4335fc
Muhammat Fandi Mayuso <>
2020-02-19 13:27:10 +0700

    project setup

---
+++ UberCloneApp/Controller/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  UberCloneApp
+//
+//  Created by Muhammat Fandi Mayuso on 19/2/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
