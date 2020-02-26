b8e82c562e14ee943e41b630c806c446546cc6f1
Muhammat Fandi Mayuso <>
2020-02-26 15:55:07 +0700

    Implement sign up method

--- UberCloneApp/Controller/SignUpController.swift
+++ UberCloneApp/Controller/SignUpController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpController: UIViewController {
     
@@ -72,6 +73,7 @@ class SignUpController: UIViewController {
         let button = AuthButton(type: .system)
         button.setTitle("Sign Up", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handlerSignUp), for: .touchUpInside)
         return button
     }()
     
@@ -101,6 +103,30 @@ class SignUpController: UIViewController {
     
     @objc func handlerShowLogin() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func handlerSignUp() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullNameTextField.text else { return }
+        let accountTypeIndex = accountTypeSegmentedControl.selectedSegmentIndex
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("Failed to register user with error \(error)")
+                return
+            }
+            
+            guard let uid = result?.user.uid else { return }
+            
+            let values = ["email": email,
+                          "fullname": fullname,
+                          "accountType": accountTypeIndex] as [String : Any]
+            
+            Database.database().reference().child("users").child(uid).updateChildValues(values) { (error, ref) in
+                print("Successfully registered user and saved data...")
+            }
+        }
     }
     
     // MARK: - Helper Functions
