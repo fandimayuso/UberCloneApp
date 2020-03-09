c995211b167297fe8d181841a7b1395ecde3e2c8
Muhammat Fandi Mayuso <>
2020-03-09 17:12:45 +0700

    Animating table view presentation and dismissal

--- UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/Controller/HomeController.swift
@@ -95,7 +95,9 @@ class HomeController: UIViewController {
         UIView.animate(withDuration: 0.5, animations: {
             self.locationInputView.alpha = 1
         }) { _ in
-            print("DEBUG: Present table view..")
+            UIView.animate(withDuration: 0.3) {
+                self.tableView.frame.origin.y = self.locationInputViewHeight
+            }
         }
     }
     
@@ -163,11 +165,14 @@ extension HomeController: LocationInputActivationViewDelegate {
 
 extension HomeController: LocationInputViewDelegate {
     func dismissLocationInputView() {
+        locationInputView.removeFromSuperview()
+        
         UIView.animate(withDuration: 0.3, animations: {
             self.locationInputView.alpha = 0
         }) { _ in
             UIView.animate(withDuration: 0.3) {
                 self.inputActivationView.alpha = 1
+                self.tableView.frame.origin.y = self.view.frame.height
             }
         }
     }
