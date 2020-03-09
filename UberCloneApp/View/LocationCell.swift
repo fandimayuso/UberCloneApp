0f6d3e179561228c4dc16823496fd120336dcbd2
Muhammat Fandi Mayuso <>
2020-03-09 16:59:57 +0700

    Setup locations table view

---
+++ UberCloneApp/View/LocationCell.swift
@@ -0,0 +1,25 @@
+//
+//  LocationCell.swift
+//  UberCloneApp
+//
+//  Created by Muhammat Fandi Mayuso on 9/3/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
