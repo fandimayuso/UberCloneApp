61d178d2888fec42e7857f1d248eb56e01311723
Muhammat Fandi Mayuso <>
2020-03-10 11:41:20 +0700

    Fetch user data

---
+++ UberCloneApp/Service/Service.swift
@@ -0,0 +1,26 @@
+//
+//  Service.swift
+//  UberCloneApp
+//
+//  Created by Muhammat Fandi Mayuso on 10/3/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import Firebase
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
+
+struct Service {
+    
+    static let shared = Service()
+    let currentUid = Auth.auth().currentUser?.uid
+    
+    func fetchUserData() {
+        REF_USERS.child(currentUid!).observeSingleEvent(of: .value) { (snapshot) in
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            guard let fullname = dictionary["fullname"] as? String else { return }
+            print("DEBUG: User full name is \(fullname)")
+        }
+    }
+}
