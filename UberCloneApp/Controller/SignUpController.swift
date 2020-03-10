93b3d8461002d4a2cc80e199ed681ea09c5c2dd5
Muhammat Fandi Mayuso <>
2020-03-10 21:31:08 +0700

    Register driver and upload location to database

--- UberCloneApp/Controller/SignUpController.swift
+++ UberCloneApp/Controller/SignUpController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import Firebase
+import GeoFire
 
 class SignUpController: UIViewController {
     
     // MARK: - Properties
+    
+    private var location = LocationHandler.shared.locationManager.location
     
     private let titleLabel: UILabel = {
         let label = UILabel()
@@ -120,18 +123,31 @@ class SignUpController: UIViewController {
             guard let uid = result?.user.uid else { return }
             
             let values = ["email": email,
-                          "fullname": fullname,
-                          "accountType": accountTypeIndex] as [String : Any]
+            "fullname": fullname,
+            "accountType": accountTypeIndex] as [String : Any]
             
-            Database.database().reference().child("users").child(uid).updateChildValues(values) { (error, ref) in
-                guard let controller = UIApplication.shared.keyWindow?.rootViewController as? HomeController else { return }
-                controller.configureUI()
-                self.dismiss(animated: true, completion: nil)
+            if accountTypeIndex == 1 {
+                let geofire = GeoFire(firebaseRef: REF_DRIVER_LOCATIONS)
+                guard let location = self.location else { return }
+                
+                geofire.setLocation(location, forKey: uid) { (error) in
+                    self.uploadUserDataAndShowHomeController(uid: uid, values: values)
+                }
             }
+            
+            self.uploadUserDataAndShowHomeController(uid: uid, values: values)
         }
     }
     
     // MARK: - Helper Functions
+    
+    func uploadUserDataAndShowHomeController(uid: String, values: [String: Any]) {
+        REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
+            guard let controller = UIApplication.shared.keyWindow?.rootViewController as? HomeController else { return }
+            controller.configureUI()
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
     
     func configureUI() {
         view.backgroundColor = .backgroundColor
---
+++ UberCloneApp/Utils/LocationHandler.swift
@@ -0,0 +1,32 @@
+//
+//  LocationHandler.swift
+//  UberCloneApp
+//
+//  Created by Muhammat Fandi Mayuso on 10/3/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationHandler()
+    var locationManager: CLLocationManager!
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+}
+
