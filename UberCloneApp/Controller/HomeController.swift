b4c509ef770c572af43987ee8ee553d6a2097059
Muhammat Fandi Mayuso <>
2020-03-15 17:45:50 +0700

    Set up user interface

--- UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/Controller/HomeController.swift
@@ -28,7 +28,7 @@ class HomeController: UIViewController {
     
     private let mapView = MKMapView()
     private let locationManager = LocationHandler.shared.locationManager
-    
+    private let rideActionView = RideActionView()
     private let inputActivationView = LocationInputActivationView()
     private let locationInputView = LocationInputView()
     private let tableView = UITableView()
@@ -157,6 +157,7 @@ class HomeController: UIViewController {
     
     func configureUI() {
         configureMapView()
+        configureRideActionView()
         
         view.addSubview(actionButton)
         actionButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 20, width: 30, height: 30)
@@ -197,6 +198,11 @@ class HomeController: UIViewController {
                 self.tableView.frame.origin.y = self.locationInputViewHeight
             }
         }
+    }
+    
+    func configureRideActionView() {
+        view.addSubview(rideActionView)
+        rideActionView.frame = CGRect(x: 0, y: view.frame.height - 300, width: view.frame.width, height: 300)
     }
     
     func configureTableView() {
