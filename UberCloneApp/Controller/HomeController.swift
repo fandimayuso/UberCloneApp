5715712717b0a4f77b810a4da348fe1d327d5a30
Muhammat Fandi Mayuso <>
2020-03-15 21:25:41 +0700

    Configure destination data

--- UberCloneApp/Controller/HomeController.swift
+++ UberCloneApp/Controller/HomeController.swift
@@ -229,8 +229,13 @@ class HomeController: UIViewController {
         }, completion: completion)
     }
     
-    func animateRideActionView(shouldShow: Bool) {
+    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil) {
         let yOrigin = shouldShow ? self.view.frame.height - self.rideActionViewHeight : self.view.frame.height
+        
+        if shouldShow {
+            guard let destination = destination else { return }
+            rideActionView.destination = destination
+        }
         
         UIView.animate(withDuration: 0.3) {
             self.rideActionView.frame.origin.y = yOrigin
@@ -414,7 +419,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             
             self.mapView.showAnnotations(annotations, animated: true)
             
-            self.animateRideActionView(shouldShow: true)
+            self.animateRideActionView(shouldShow: true, destination: selectedPlacemark)
         }
     }
 }
