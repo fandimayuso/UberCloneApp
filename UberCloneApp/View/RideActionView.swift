afcbc66daeed93ef6387d2a3b2a3edc6842deaec
Muhammat Fandi Mayuso <>
2020-03-16 22:13:55 +0700

    Requesting a ride

--- UberCloneApp/View/RideActionView.swift
+++ UberCloneApp/View/RideActionView.swift
@@ -9,6 +9,10 @@
 import UIKit
 import MapKit
 
+protocol RideActionViewDelegate: class {
+    func uploadTrip(_ view: RideActionView)
+}
+
 class RideActionView: UIView {
 
     // MARK: - Properties
@@ -19,6 +23,8 @@ class RideActionView: UIView {
             addressLabel.text = destination?.address
         }
     }
+    
+    weak var delegate: RideActionViewDelegate?
     
     private let titleLabel: UILabel = {
         let label = UILabel()
@@ -114,7 +120,7 @@ class RideActionView: UIView {
     // MARK: - Actions
     
     @objc func actionButtonPressed() {
-        print("123")
+        delegate?.uploadTrip(self)
     }
     
 
