f5d33b2f2dd08ae16ff13fba1bef7a5d9b8f9019
Muhammat Fandi Mayuso <>
2020-04-08 21:45:00 +0700

    Populate table view with search results

--- UberCloneApp/Controller/Settings/AddLocationController.swift
+++ UberCloneApp/Controller/Settings/AddLocationController.swift
@@ -17,7 +17,11 @@ class AddLocationController: UITableViewController {
     
     private let searchBar = UISearchBar()
     private let searchCompleter = MKLocalSearchCompleter()
-    private var searchResults = [MKLocalSearchCompletion]()
+    private var searchResults = [MKLocalSearchCompletion]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     private let type: LocationType
     private let location: CLLocation
     
@@ -67,6 +71,8 @@ class AddLocationController: UITableViewController {
     }
 }
 
+// MARK: - TableViewDelegate and Datasource
+
 extension AddLocationController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchResults.count
@@ -74,14 +80,25 @@ extension AddLocationController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        let result = searchResults[indexPath.row]
+        cell.textLabel?.text = result.title
+        cell.detailTextLabel?.text = result.subtitle
         return cell
     }
 }
 
+// MARK: - UISearchBarDelegate
+
 extension AddLocationController: UISearchBarDelegate {
-    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
 }
 
+// MARK: - MKLocalSearchCompleterDelegate
+
 extension AddLocationController: MKLocalSearchCompleterDelegate {
-    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+    }
 }
