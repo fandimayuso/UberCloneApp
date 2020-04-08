759686914a0732dfc640ec7ef00030c131ac2263
Muhammat Fandi Mayuso <>
2020-04-08 10:58:03 +0700

    Get start with adding saved locations

--- UberCloneApp/Controller/Settings/SettingsController.swift
+++ UberCloneApp/Controller/Settings/SettingsController.swift
@@ -34,6 +34,7 @@ class SettingsController: UITableViewController {
     // MARK: - Properties
     
     private let user: User
+    private let locationManager = LocationHandler.shared.locationManager
     
     private lazy var infoHeader: UserInfoHeader = {
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
@@ -71,6 +72,7 @@ class SettingsController: UITableViewController {
         tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.backgroundColor = .white
         tableView.tableHeaderView = infoHeader
+        tableView.tableFooterView = UIView()
     }
     
     func configureNavigationBar() {
@@ -120,6 +122,9 @@ extension SettingsController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let type = LocationType(rawValue: indexPath.row) else { return }
-        print("DEBUG: Type is \(type.description)")
+        guard let location = locationManager?.location else { return }
+        let controller = AddLocationController(type: type, location: location)
+        let nav = UINavigationController(rootViewController: controller)
+        present(nav, animated: true, completion: nil)
     }
 }
